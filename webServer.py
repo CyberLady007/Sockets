133ba06c2a7266adbdec2421b6c623f81ba6b7b6
CyberLady007 <af4640@nyu.edu>
2023-09-23 19:35:20 +0200

    new try

--- webServer.py
+++ webServer.py
@@ -33,6 +33,18 @@ def webServer(port=13331):
                      # This variable can store the headers you want to send for any valid or invalid request.   What header should be sent for a response that is ok?
                      # Fill in start
             outputdata = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nServer: MyServer\r\nConnection: close\r\n\r\n"
+            outputdata+= """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>HelloWorld</title>
+</head>
+<body>
+    <p>Hello World!</p>
+</body>
+</html>
+"""
             connectionSocket.send(outputdata.encode('utf-8'))
                      # Content-Type is an example on how to send a header as bytes. There are more!
 
